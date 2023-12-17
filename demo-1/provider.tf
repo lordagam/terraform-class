020b84a110b098c22688d9a2322d3b582b978d11
lidor gerstel <lidorg@dev-operations.io>
2023-12-17 18:50:23 +0200

    added all demo classes

---
+++ demo-1/provider.tf
@@ -0,0 +1,6 @@
+# configure the AWS provider
+provider "aws" {
+    access_key = "AKIAU77SCMU3CCVPUOXB"
+    secret_key = "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"
+    region     = "eu-central-1"
+}
