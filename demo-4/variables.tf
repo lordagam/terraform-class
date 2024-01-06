add9376225dc325c712996b8b27ec50eed9ce66f
joseph agam <127337524+lordagam@users.noreply.github.com>
2024-01-06 18:11:22 +0200

    Update variables.tf

--- demo-4/variables.tf
+++ demo-4/variables.tf
@@ -1,13 +1,13 @@
 variable "region" {
     description = "Insert your AWS Region Code"
-    default     = "eu-central-1"
+    default     = "eu-west-3"
 }
 
 variable "AMIS" {
     description = "choose your ami depends on region"
     type = map(string)
     default = {
-        us-east-1    = "ami-0c7217cdde317cfec"
+        eu-west-3    = "ami-01d21b7be69801c2f"
         eu-west-1    = "ami-0905a3c97561e0b69"
         eu-central-1 = "ami-0faab6bdbac9486fb"
     }
